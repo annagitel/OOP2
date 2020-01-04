55cc71cf2096b0eb36c951afc1aa693773feab74
yanashayev <yana.shayev123@gmail.com>
2020-01-04 20:52:02 +0200

    update gui

--- src/gui/Graph_GUI.java
+++ src/gui/Graph_GUI.java
@@ -6,22 +6,28 @@ import dataStructure.*;
 public class Graph_GUI {
 
     public static void main(String[] args) {
-       // DGraph g = new DGraph();
-       // g.addNode("1,3,0");
-        //g.addNode("4,14,0");
-        //g.addNode("14,11,0");
-        //g.addNode("17,16,0");
-        //g.addNode("21,2,0");
+        DGraph g = new DGraph();
+        NodeData n1= new NodeData(1);
+        NodeData n2= new NodeData(2);
+        NodeData n3= new NodeData(3);
+        NodeData n4= new NodeData(4);
+        NodeData n5= new NodeData(5);
+        g.addNode(n1);
+        g.addNode(n2);
+        g.addNode(n3);
+        g.addNode(n4);
+        g.addNode(n5);
 
-       // g.connect(1,2,2);
-       // g.connect(2,4,1);
-        //g.connect(4,5,7);
-       // g.connect(1,3,3);
-        //g.connect(3,5,4);
-        //g.connect(5,1,6);
-       // g.connect(1,5,1);
 
-       // StdDraw.drawGraph(g);
+        g.connect(1,2,2);
+        g.connect(2,4,1);
+        g.connect(4,5,7);
+        g.connect(1,3,3);
+        g.connect(3,5,4);
+        g.connect(5,1,6);
+        g.connect(1,5,1);
+
+        StdDraw.drawGraph(g);
 
     }
 }
