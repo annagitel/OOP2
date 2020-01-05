7cfacaa2c89f90cd58c5e3cc5405029e482672eb
asandler <annagitel39@gmail.com>
2020-01-05 16:11:00 +0200

    added distance popup

--- src/utils/StdDraw.java
+++ src/utils/StdDraw.java
@@ -880,12 +880,15 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
 						JOptionPane.ERROR_MESSAGE);
 			}
 		}
-
+		double dist = gui_algo.shortestPathDist(s,d);
 		List<node_data> list = (ArrayList<node_data>) gui_algo.shortestPath(s,d);
 		for (node_data n:list) {
 			drawNode(n, Color.blue);
 		}
+		JOptionPane.showMessageDialog(frame,
+				"The distanse is "+String.valueOf(dist));
 	}
+
 
 	public static void tspDraw(){
 		drawDefault();
