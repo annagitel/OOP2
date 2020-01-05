afc8c19c5a9db275a0661272ada9e5e4299c78a9
yanashayev <yana.shayev123@gmail.com>
2020-01-05 17:25:54 +0200

    update gui

---
+++ src/Tests/algoTest.java
@@ -0,0 +1,8 @@
+package Tests;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import algorithms.*;
+
+public class algoTest {
+
+}
--- src/gui/Graph_GUI.java
+++ src/gui/Graph_GUI.java
@@ -11,7 +11,7 @@ public class Graph_GUI {
         Point3D p2=new Point3D(100,25,0);
         Point3D p3=new Point3D(30,1,0);
         Point3D p4=new Point3D(13,40,0);
-        Point3D p5=new Point3D(50,7,0);
+        Point3D p5=new Point3D(80,10,0);
         NodeData n1= new NodeData(1,p1);
         NodeData n2= new NodeData(2,p2);
         NodeData n3= new NodeData(3,p3);
@@ -23,12 +23,15 @@ public class Graph_GUI {
         g.addNode(n4);
         g.addNode(n5);
 
-
+        g.connect(4,3,2);
+        g.connect(2,3,1);
+        g.connect(4,5,7);
+        g.connect(5,2,3);
+        g.connect(3,5,4);
+        g.connect(5,4,6);
         g.connect(1,2,2);
         g.connect(2,4,1);
-        g.connect(4,5,7);
         g.connect(1,3,3);
-        g.connect(3,5,4);
         g.connect(5,1,6);
         g.connect(1,5,1);
 
